144b6c11211a71aa12d1c97241f466ceb6d48ac7
Camilo Reyes <reyesc@paylocity.com>
2023-11-01 13:41:42 -0500

    Initial commit

---
+++ src/PaylocityPocWorkerAsync/PaylocityPocWorkerAsyncStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace PaylocityPocWorkerAsync
+{
+    public class PaylocityPocWorkerAsyncStack : Stack
+    {
+        internal PaylocityPocWorkerAsyncStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
